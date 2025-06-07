d1a505c14228208f07fa75d4972193eaaf3114d4
EmanuelVargas11 <vargasriquelmege@gmail.com>
2025-06-07 11:32:57 -0300

    ejercicio 1, en una sola linea

--- src/app.js
+++ src/app.js
@@ -2,17 +2,18 @@
 
 const numeros = [1, 2, 3, 4, 5]
 
-// const numerosCuadrados = numeros.map(numero => {
-//     return numero * numero
-// })
-
-// console.log(numerosCuadrados);
-
 const cuadradosDeNumeros = numeros.map(numero => {
-    
-    let numCuadrado = numero * numero;
-
-    return numCuadrado
+    return numero * numero
 })
 
 console.log(cuadradosDeNumeros);
+
+// const cuadradosDeNumeros = numeros.map(numero => {
+    
+//     let numCuadrado = numero * numero;
+
+//     return numCuadrado
+// })
+
+// console.log(cuadradosDeNumeros);
+
